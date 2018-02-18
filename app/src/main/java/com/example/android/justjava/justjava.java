02d531c4f0030232408fc05a929479cce5f82eb1
Dorian <dgrove79@gmail.com>
2018-02-17 19:41:10 -0500

    Additions: Toppings selections
    Fix:
    Will fix the rest of the dimens later.
    
    Signed-off-by: Dorian <dgrove79@gmail.com>

--- app/src/main/java/com/example/android/justjava/justjava.java
+++ app/src/main/java/com/example/android/justjava/justjava.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import java.text.NumberFormat;
 
 
 public class justjava extends AppCompatActivity {
@@ -19,32 +18,43 @@ public class justjava extends AppCompatActivity {
 
     public void increment(View view){
         quantity = quantity + 1;
-        display (quantity);
-        displayPrice (quantity * 5);
+        TextView quantityTextView = findViewById (R.id.quantity_text_view);
+        displayQuantity (quantity);
+
     }
     public void decrement(View view){
         quantity = quantity - 1;
-        display (quantity);
-        displayPrice (quantity * 5);
+        TextView quantityTextView = findViewById (R.id.quantity_text_view);
+        displayQuantity (quantity);
+
     }
-    public void submitOrder(View view){
-        String priceMessage = "Total: $ " + (quantity *5);
-        priceMessage = priceMessage + " \nThank You!";
-        displayMessage (priceMessage);
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        TextView priceTextView = findViewById(R.id.order_summary_text_view);
+        String priceMessage = "Hello Dorian \nYour total for \n" + quantity + " cup(s) of coffee is $" + calculatePrice() + "\n Thank you!";
+        priceTextView.setText(priceMessage);
     }
 
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById (R.id.quantity_text_view);
-        quantityTextView.setText ("" + number);
+    /**
+     * Calculates the price of the order.
+     *
+     *@ return total price
+     *
+     */
+    private int calculatePrice() {
+        return quantity * 5;
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private void displayQuantity(int number) {
+        TextView quantityTextView = findViewById (R.id.quantity_text_view);
+        quantityTextView.setText (String.format ("%d", number));
     }
+
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
 }
